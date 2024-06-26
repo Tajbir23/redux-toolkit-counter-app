d3a35b877a2e09c72f28aca6f9776c7169bf8cdd
Tajbir islam <studenttajbirislam@gmail.com>
2024-06-26 09:52:21 +0600

    note

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CounterView from "./features/counter/CounterView"
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Counter app</h1>
+      <CounterView />
+    </div>
+  )
+}
+
+export default App
